dfb8f27a416cf12cb791688d9f4dcba41aca6e2d
Mateusz Pawlowski <mateusz.pawlowski@pearson.com>
2013-07-05 13:06:56 +0100

    0.0.3 adding xml-simple to dependencies

--- rs-api-tools.gemspec.rb
+++ rs-api-tools.gemspec.rb
@@ -1,15 +1,16 @@
 Gem::Specification.new do |s|
   s.name        = 'rs-api-tools'
-  s.version     = '0.0.2'
-  s.date        = '2013-06-12'
+  s.version     = '0.0.3'
+  s.date        = '2013-07-05'
   s.summary     = "rs-api-tools"
   s.description = "RightScale API Command Line Tools."
-  s.authors     = ["Chris Fordham"]
-  s.email       = 'chris@xhost.com.au'
+  s.authors     = ["Chris Fordham", "Mateusz Pawlowski - Generik Ltd"]
+  s.email       = 'mateusz@generik.co.uk'
   s.bindir      = 'bin'
   s.executables = Dir.entries(s.bindir) - [".", "..", '.gitignore']
   s.homepage    =
-    'https://github.com/flaccid/rs-api-tools'
+    'https://github.com/generiklimited/rs-api-tools'
   s.add_dependency "json", ">= 1.4.4", "<= 1.6.1"
   s.add_dependency 'rest_connection'
+  s.add_dependency 'xml-simple'
 end
